d527c94f2791bae3a92a53303ef8143836b28838
Oliver Loftus <oliverloftus1@gmail.com>
2023-01-12 10:42:55 +0000

    added POST note route and model NOT WORKING

--- models/patients.js
+++ models/patients.js
@@ -1,4 +1,4 @@
-import { query } from '../db/index.js';
+import { query } from "../db/index.js";
 
 export async function getPatientByID(id) {
   const results = await query(
@@ -15,8 +15,8 @@ export async function getPatientsByCarerID(carer_id) {
   console.log(carer_id);
   const results = await query(
     // `SELECT *
-    // FROM patients 
-    // LEFT JOIN carers 
+    // FROM patients
+    // LEFT JOIN carers
     // ON patients.carer_id = carers.id
     // WHERE carers.id = $1;`,
     // [carer_id]
@@ -29,7 +29,6 @@ export async function getPatientsByCarerID(carer_id) {
     ON cp.patient_id = pat.patient_id
     WHERE cp.carer_id = $1;`,
     [carer_id]
-
   );
   const patientsArr = results.rows;
   return patientsArr;
@@ -44,4 +43,29 @@ export async function getNotesByPatientID(patient_id) {
   );
   const notesArr = results.rows;
   return notesArr;
+}
+
+export async function addNote(NoteObj) {
+  const result = await query(
+    `INSERT INTO notes (patient_id, carer_id, text_content, type_note, time_of_input, has_been_seen) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`,
+    [
+      NoteObj.patient_id,
+      NoteObj.carer_id,
+      NoteObj.text_content,
+      NoteObj.type_note,
+      NoteObj.time_of_input,
+      NoteObj.has_been_seen,
+    ]
+  );
+  // const newNoteObj = {
+  //   patient_id: 1,
+  //   carer_id: 1,
+  //   text_content: `This is a note`,
+  //   type_note: `Incident`,
+  //   time_of_input: `2023/01/12`,
+  //   has_been_seen: true,
+  // };
+
+  const newNoteObj = result.rows[0];
+  return newNoteObj;
 }
--- routes/patients.js
+++ routes/patients.js
@@ -1,26 +1,39 @@
-import { Router } from 'express';
-import { getNotesByPatientID, getPatientByID } from '../models/patients.js';
+import { Router } from "express";
+import {
+  getNotesByPatientID,
+  getPatientByID,
+  addNote,
+} from "../models/patients.js";
 
 const router = Router();
 
 // GET single patient's info
-router.get('/:patient_id', async function (req, res) {
+router.get("/:patient_id", async function (req, res) {
   const patient = await getPatientByID(req.params.patient_id);
   res.json({
     success: true,
-    payload: patient
+    payload: patient,
   });
-})
+});
 
 // GET single patient's notes
-router.get('/:patient_id/notes', async function (req, res) {
-  const notes = await getNotesByPatientID(req.params.patient_id)
+router.get("/:patient_id/notes", async function (req, res) {
+  const notes = await getNotesByPatientID(req.params.patient_id);
   console.log(notes);
   res.json({
     success: true,
-    payload: notes
+    payload: notes,
   });
-});  
+});
 
-    
+// POST new patient note
+router.post("/patients/:patient_id/notes", async function (req, res) {
+  // not sure if we need to pass in patient_id
+  const note = await addNote(req.body);
+  res.json({
+    success: true,
+    payload: note,
+  });
+});
+
 export default router;
