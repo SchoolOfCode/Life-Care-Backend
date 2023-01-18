87c510a9b38e4e8aac36319f66af8f4419e5580d
Ashraf Abdelrahman <96283360+AshrafAKRahman@users.noreply.github.com>
2023-01-18 14:04:21 +0000

    created the server.js, and removed the try catches to get a failing test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import carersRouter from "./routes/carers.js";
 
 
 const app = express();
-const port = 3005;
+
 
 // Middlewares
 app.use(cors("*"));
@@ -14,9 +14,7 @@ app.use(morgan("dev"));
 app.use(express.static("public"));
 app.use(express.json());
 
-app.listen(port, () => {
-	console.log(`app listening on port ${port}`);
-});
+
 
 // Routing
 app.use("/api/patients", patientsRouter);
--- routes/carers.test.js
+++ routes/carers.test.js
@@ -4,42 +4,33 @@ import app from "../app";
 
 
 test('it gets all the carers', async()=>{
-    try {
+    
         const response = await supertest(app).get("/api/carers")
         expect(response.status).toBe(200);
         expect (response.body).toStrictEqual ({
             success: true,
             payload: expect.any(Array)
         })
-
-    } catch (error){
-        console.log(error);
-    }
     
 })
 
 test('it gets carers by ID', async()=> {
-    try {
+
         const response = await supertest(app).get("/api/carers/:carer_id")
         expect(response.status).toBe(200);
         expect (response.body).toStrictEqual({
             success: true,
             payload: expect.any(Object)
         })
-    }catch (error){
-        console.log(error);
-    }
 })
 
 test("Getting array of assigned patients", async()=>{
-    try {
+    
         const response = await supertest(app).get("/api/carers/:carer_id/patients")
         expect(response.status).toBe(200);
         expect (response.body).toStrictEqual({
             success: true,
             payload: expect.any(Array)
         })
-    } catch (error){
-        console.log(error);
-    }
+
 })
