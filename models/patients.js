f309c235df243b8576008c1cbe110ba9734b6c7a
Ashraf Abdelrahman <96283360+AshrafAKRahman@users.noreply.github.com>
2023-01-20 06:45:43 +0000

    moved get patient by carer ID to carer models file, updated the env variable for the port

--- models/patients.js
+++ models/patients.js
@@ -1,52 +1,5 @@
 import { query } from "../db/index.js";
 
-<<<<<<< HEAD
-=======
-export async function getPatientByID(id) {
-  const results = await query(
-    `SELECT * 
-    FROM patients 
-    WHERE patient_id = $1;`,
-    [id]
-  );
-  const patient = results.rows[0];
-  return patient;
-}
-
-export async function getPatientsByCarerID(carer_id) {
-  const results = await query(
-    // `SELECT *
-    // FROM patients
-    // LEFT JOIN carers
-    // ON patients.carer_id = carers.id
-    // WHERE carers.id = $1;`,
-    // [carer_id]
-
-    // !Below is SQL Query for if we use the junction table
-
-    `SELECT pat.*
-    FROM carer_patients AS cp
-    JOIN patients AS pat 
-    ON cp.patient_id = pat.patient_id
-    WHERE cp.carer_id = $1;`,
-    [carer_id]
-  );
-  const patientsArr = results.rows;
-  return patientsArr;
-}
-
-export async function getNotesByPatientID(patient_id) {
-  const results = await query(
-    `SELECT content, incidents, additional, time_stamp, seen
-    FROM notes
-    WHERE patient_id = $1;`,
-    [patient_id]
-  );
-  const notesArr = results.rows;
-  return notesArr;
-}
-
->>>>>>> 135b7f45fc97668620f56bf05f216c77322f40bd
 export async function addNote(NoteObj) {
   const result = await query(
     `INSERT INTO notes (patient_id, carer_id, text_content, type_note, time_of_input, has_been_seen) VALUES ($1, $2, $3, $4, $5, $6) RETURNING *;`,
